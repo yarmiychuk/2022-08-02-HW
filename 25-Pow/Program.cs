3c03e06fe935c9ac4c10a27765b9dd30f47566e1
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-08-08 01:32:56 +0300

    Syntax edit

--- 25-Pow/Program.cs
+++ 25-Pow/Program.cs
@@ -20,7 +20,7 @@ bool IsCorrectInput(double num)
     return (num > 0);
 }
 
-Console.WriteLine("Программа для вычисление степени от числа");
+Console.WriteLine("Программа для вычисления степени от числа");
 Console.Write("Введите число: ");
 double number = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите натуральную степень, в которую нужно возвести число: ");
