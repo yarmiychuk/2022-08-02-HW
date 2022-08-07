ebc500ff1c1a0e4595a57d9a26fc60d2215e72b6
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-08-08 01:11:12 +0300

    Pow program added

--- 25-Pow/Program.cs
+++ 25-Pow/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.
+*/
+
+double GetPow(double number, double pow)
+{
+    double result = 1;
+
+    for (int i = 0; i < pow; i++)
+    {
+        result *= number;
+    }
+
+    return result;
+}
+
+bool IsCorrectInput(double num)
+{
+    return (num > 0);
+}
+
+Console.WriteLine("Программа для вычисление степени от числа");
+Console.Write("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральную степень, в которую нужно возвести число: ");
+
+double pow;
+while (!IsCorrectInput(pow = Convert.ToInt32(Console.ReadLine())))
+{
+    Console.Write("Вы ввели неправильное число! Повторите ввод: ");
+}
+
+Console.WriteLine($"{number}^{pow} = {GetPow(number, pow)}");
